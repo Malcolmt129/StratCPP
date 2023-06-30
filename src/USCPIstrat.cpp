0a2c20a60f53b382cd333997762676d9dba44d97
Malcolmt129 <malcolm.t129@gmail.com>
2023-06-29 21:35:47 -0400

    made the .h and beginning of implementation of USCPIStrat

--- src/USCPIstrat.cpp
+++ src/USCPIstrat.cpp
@@ -1,9 +1,35 @@
-#include 
+#include "USCPIstrat.h"
 
-class USCPIstrat {
-    public: 
+ 
         
-
-    
+std::vector<std::string> USCPIstrat::getlastXevents(int occurances) {
+            
+    return std::vector<std::string>();
 }
+
+std::vector<std::string> USCPIstrat::creatPriceList() {
+            
+    return std::vector<std::string>();
+}
+
+std::unordered_map<std::string, std::string> USCPIstrat::getReleases(std::string startDate, std::string endDate) {
+            
+    return std::unordered_map<std::string, std::string>();
+}
+
+std::unordered_map<std::string, std::string> USCPIstrat::addReleasesToDB(int releases) {
+            
+    return std::unordered_map<std::string, std::string>();
+}
+
+std::vector<std::vector<std::string>> USCPIstrat::getCandleData(std::string pair, int occurances) {
+            
+    return std::vector<std::vector<std::string>>();
+}
+
+std::vector<std::vector<std::string>> USCPIstrat::form_candleList(std::string response) {
+            
+    return std::vector<std::vector<std::string>>();
+}
+
 
--- src/USCPIstrat.h
+++ src/USCPIstrat.h
@@ -1,13 +1,36 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+
+//import MongoDB api thingy
 
 
 class USCPIstrat {
 
 	public:
 		
+		USCPIstrat() {
+			//MongoDB connection placeholder
+		}
+		
+		~USCPIstrat() {}
+
+		std::vector<std::string> getlastXevents(int occurances);
+		
+		std::vector<std::string> creatPriceList();
+
+		std::unordered_map<std::string,std::string> getReleases(std::string startDate, std::string endDate);
+
+		std::unordered_map<std::string, std::string> addReleasesToDB(int releases);
+
+		std::vector<std::vector<std::string>> getCandleData(std::string pair, int occurances);
+
+		std::vector<std::vector<std::string>> form_candleList(std::string response);
 
 	private:
-		std::vector<std::string> currency_pairs = { "EUR_USD" };
+		std::vector<std::string> currency_pairs;
 		
 
 };
