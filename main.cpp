1f9be61e4a844af9987bbdac5d33f88d91901ebf
Malcolmt129 <malcolm.t129@gmail.com>
2023-07-05 23:22:59 -0400

    idk

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "This is the main executing \n"; 
+    return 0;
+}
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    
-
-    std::cout << "This is the main file running" << std::endl;
-    
-    return 0;
-}
